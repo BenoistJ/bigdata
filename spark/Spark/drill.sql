8d4e62eaea3241a14c768e2310cecbdce2ccb881
Benoist <benpresso@gmail.com>
2017-02-23 15:43:31 +0100

    ...

--- spark/Spark/drill.sql
+++ spark/Spark/drill.sql
@@ -1,7 +1,6 @@
 SELECT monthDate, count(clientIpAddress) AS c
 FROM (
-  SELECT DISTINCT monthDate, clientIpAddress
-  FROM dfs.`/user/ubuntu/full/prepared/data.csv`
-  WHERE domainName = 'www.mywebsite.com')
+  SELECT DISTINCT columns[3] as monthDate, columns[0] as clientIpAddress
+  FROM dfs.`/datalake/prepared/big/data.csv`)
 GROUP BY monthDate
 ORDER BY monthDate ASC;
