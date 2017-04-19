da0ded7f73a13eabef73a07e2626538297556516
Benoist <benoist.jamin@gmail.com>
2017-04-19 16:47:12 +0200

    ...

--- spark/spark-example.sh
+++ spark/spark-example.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-spark-submit --class org.apache.spark.examples.SparkPi --name TestPI --master yarn-cluster --executor-cores 2 --executor-memory 256m /opt/cloudera/parcels/CDH/jars/spark-examples*.jar 900
+spark-submit --class org.apache.spark.examples.SparkPi --name SparkPI --master yarn-cluster --executor-cores 2 --executor-memory 256m /opt/cloudera/parcels/CDH/jars/spark-examples*.jar 900
