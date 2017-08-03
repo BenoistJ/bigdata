dae27a2f545b547482c53aa361f48c7020619cd9
Benoist <benoist.jamin@gmail.com>
2017-08-03 17:02:35 +0200

    ...

--- spark/demo/main.scala
+++ spark/demo/main.scala
@@ -3,8 +3,11 @@ rdd1.count
 
 val rdd2 = rdd1.filter(_.contains("www.mydemo.fr"))
 val rdd3 = rdd2.map(_.split(";"))
-val rdd4 = rdd3.coalesce(4)
-val rdd5 = rdd4.map(cols => (cols(0), 1))
-val rdd6 = rdd5.reduceByKey(_ + _)
-val rdd7 = rdd6.sortBy(_._2, ascending=false)
-rdd7.take(10).foreach(println)
+val rdd33 = rdd3.coalesce(4)
+val rdd4 = rdd3.map(cols => (cols(0), 1))
+val rdd5 = rdd4.reduceByKey(_ + _)
+val rdd6 = rdd5.sortBy(_._2, ascending=false)
+rdd6.foreach(println)
+
+rdd6.coalesce(1).foreach(println)
+rdd6.collect.foreach(println)
